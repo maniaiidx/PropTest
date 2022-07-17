b0495f60b02c0a5e7b31297a64fc4bc71b61d20d
AzlightRimlight <q.breadmarch+3395@gmail.com>
2022-07-17 11:56:16 +0900

    Rayfire1.50ChangeLogに追記、鉄筋用のTmpプレハブ作成

---
+++ Assets/_Azlight/Tmp_AddRayfireScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Presets;
+using RayFire;
+
+public class Tmp_AddRayfireScript : MonoBehaviour
+{
+    public Preset PresetSource;
+
+    [SerializeField, Space(20)]
+
+    [Button(nameof(addRayfireRigid), "Add RayfireRigid Component")]
+    public bool dummy;//属性でボタンを作るので何かしら宣言が必要。使わない
+    void addRayfireRigid()
+    {
+        //Preset preset = new Preset(PresetSource);
+        GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("Tmp_building");
+        for (int i = 0; i < gameObjects.Length; i++)
+        {
+            RayfireRigid RFrigid = gameObjects[i].transform.GetComponent<RayfireRigid>();
+            if (RFrigid == null) //RayfireRigidが付与されてなければ以下を実行
+            {
+                RFrigid = gameObjects[i].AddComponent<RayfireRigid>();
+                PresetSource.ApplyTo(RFrigid) ;
+            }
+            else
+            {
+                Debug.Log("RayfireRigid is exist.");
+            }
+        }
+    }
+
+    [SerializeField, Space(20)]
+
+    /*〇構想について
+     　CopyFileをgameObjects[]にforeachで付与する*/
+
+
+    public GameObject[] gameObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
