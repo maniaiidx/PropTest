8ca8c6b63efde6b45a19b7101b6281d94760c752
MJGTS <maniaiidx10@gmail.com>
2022-07-17 14:23:06 +0900

    ・Azlightさんのスクリプトに変数追加

--- Assets/_Azlight/Tmp_AddRayfireScript.cs
+++ Assets/_Azlight/Tmp_AddRayfireScript.cs
@@ -8,6 +8,10 @@ public class Tmp_AddRayfireScript : MonoBehaviour
 {
     public Preset PresetSource;
 
+    public string searchTagName = "Tmp_building";
+
+    public bool isOverride = false;
+
     [SerializeField, Space(20)]
 
     [Button(nameof(addRayfireRigid), "Add RayfireRigid Component")]
@@ -15,7 +19,7 @@ public class Tmp_AddRayfireScript : MonoBehaviour
     void addRayfireRigid()
     {
         //Preset preset = new Preset(PresetSource);
-        GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("Tmp_building");
+        GameObject[] gameObjects = GameObject.FindGameObjectsWithTag(searchTagName);
         for (int i = 0; i < gameObjects.Length; i++)
         {
             RayfireRigid RFrigid = gameObjects[i].transform.GetComponent<RayfireRigid>();
@@ -27,27 +31,32 @@ public class Tmp_AddRayfireScript : MonoBehaviour
             else
             {
                 Debug.Log("RayfireRigid is exist.");
+                if (isOverride)
+                {
+                    Debug.Log("上書きしました");
+                    PresetSource.ApplyTo(RFrigid);
+                }
             }
         }
     }
 
-    [SerializeField, Space(20)]
+    //[SerializeField, Space(20)]
 
-    /*〇構想について
-     　CopyFileをgameObjects[]にforeachで付与する*/
+    ///*〇構想について
+    // 　CopyFileをgameObjects[]にforeachで付与する*/
 
 
-    public GameObject[] gameObjects;
+    //public GameObject[] gameObjects;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    //// Start is called before the first frame update
+    //void Start()
+    //{
         
-    }
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
+    //// Update is called once per frame
+    //void Update()
+    //{
         
-    }
+    //}
 }
